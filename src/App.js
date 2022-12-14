9917d8084a22170df955fe691195eeb3e414d7a5
Mónica R <a20monicarr@gmail.com>
2022-12-14 12:50:02 +0100

    añadida estructura inicial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import error from './pages/error'
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Link to="/nosotros">Nosotros</Link>
     </div>
+      <Routes>
+        <Route path='/nosotros' element={<Nosotros/>}></Route>
+        <Route path='#'element ={error}></Route>
+      </Routes>
+      </BrowserRouter>
   );
 }
 
